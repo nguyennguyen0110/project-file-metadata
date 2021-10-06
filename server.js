5f71ee7bf8ed1419c4860fa9c4b9d900ada5be19
nguyennguyen0110 <85159841+nguyennguyen0110@users.noreply.github.com>
2021-10-06 09:33:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
 var app = express();
+
+//Import multer for getting file upload
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +15,11 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port: ' + port)
 });
